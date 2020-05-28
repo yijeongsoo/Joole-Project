b0537f0744e387d3441be26483e73ad0fb3989b9
HuiBeom <khuibeom@gmail.com>
2020-05-27 19:06:00 -0700

    Setting up the pages

--- JooleStoreApp/App_Start/RouteConfig.cs
+++ JooleStoreApp/App_Start/RouteConfig.cs
@@ -13,10 +13,46 @@ namespace JooleStoreApp
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            // Default
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Login", action = "Index", id = UrlParameter.Optional }
+            );
+
+            // Login
+            routes.MapRoute(
+                name: "Login",
+                url: "Login",
+                defaults: new { controller = "Login", action = "Index", id = UrlParameter.Optional }
+            );
+
+            // Search
+            routes.MapRoute(
+                name: "Search",
+                url: "Search",
+                defaults: new { controller = "Search", action = "Index", id = UrlParameter.Optional }
+            );
+
+            // Product Detail
+            routes.MapRoute(
+                name: "Product Detail",
+                url: "ProductDetail",
+                defaults: new { controller = "ProductDetail", action = "Index", id = UrlParameter.Optional }
+            );
+
+            // Product Summary
+            routes.MapRoute(
+                name: "Product",
+                url: "ProductSummary",
+                defaults: new { controller = "ProductSummary", action = "Index", id = UrlParameter.Optional }
+            );
+
+            // Compare Product
+            routes.MapRoute(
+                name: "Compare Product",
+                url: "CompareProduct",
+                defaults: new { controller = "CompareProduct", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ JooleStoreApp/Controllers/CompareProductController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JooleStoreApp.Controllers
+{
+    public class CompareProductController : Controller
+    {
+        // GET: CompareProduct
+        public ActionResult Index()
+        {
+            return View("CompareProduct");
+        }
+    }
+}
