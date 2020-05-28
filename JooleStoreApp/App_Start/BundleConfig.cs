e41d282225d4e322e3aba325889c4d6eab469f89
HuiBeom <khuibeom@gmail.com>
2020-05-27 19:58:04 -0700

    Implemented Layout Page, Added Image folder

--- JooleStoreApp/App_Start/BundleConfig.cs
+++ JooleStoreApp/App_Start/BundleConfig.cs
@@ -24,6 +24,7 @@ namespace JooleStoreApp
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/JooleStyles/Layout.css",
                       "~/Content/site.css"));
         }
     }
