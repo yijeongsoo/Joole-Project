c7bfb8545e2d460691ce3f18d9cd9d6188a9506b
heavymetalani <apimple@stevens.edu>
2020-05-31 20:37:59 -0500

    Added UI for the products

--- JooleStoreApp/Controllers/ProductSummaryController.cs
+++ JooleStoreApp/Controllers/ProductSummaryController.cs
@@ -13,12 +13,12 @@ namespace JooleStoreApp.Controllers
         public ActionResult Index()
         {
             List<Product> prodList = new List<Product>();
-            Product prod = new Product{ ProductId = 1, SubcategoryId = 1, ManufacturerId = 1, ProductName = "Fan", Model = "Royal", ModelYear = 1996 , Series="1" , ProductImage=""};
-            Product prod2 = new Product { ProductId = 2, SubcategoryId = 2, ManufacturerId = 2, ProductName = "Fan2", Model = "Feudal", ModelYear = 1920, Series = "2", ProductImage = "" };
+            Product prod = new Product{ ProductId = 1, SubcategoryId = 1, ManufacturerId = 1, ProductName = "Havels", Model = "Royal Model", ModelYear = 1996 , Series="Series 500" , ProductImage=""};
+            Product prod2 = new Product { ProductId = 2, SubcategoryId = 2, ManufacturerId = 2, ProductName = "Philips", Model = "Feudal Model", ModelYear = 1920, Series = "Series 10", ProductImage = "" };
             prodList.Add(prod);
             prodList.Add(prod2);
 
-            return View("ProductSummary", prodList);
+            return View(prodList);
         }
     }
 }
